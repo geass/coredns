7cb4143ae86bd2effd67c4ace4d79b12b609bf60
John Belamaric <jbelamaric@infoblox.com>
2018-05-04 14:56:13 -0400

    Review feedback:  * Removed client (will move to another repo)  * Use new log functions  * Change watchChan to be for string not []string  * Rework how k8s plugin stores watch tracking info to simplify  * Normalize the qname on watch request

--- plugin/kubernetes/setup.go
+++ plugin/kubernetes/setup.go
@@ -117,7 +117,6 @@ func ParseStanza(c *caddy.Controller) (*Kubernetes, error) {
 		initEndpointsCache: true,
 		ignoreEmptyService: false,
 		resyncPeriod:       defaultResyncPeriod,
-		watched:            k8s.watched,
 		endpointNameMode:   false,
 	}
 	k8s.opts = opts
